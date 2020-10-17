df19c62df3b58601dd1923c8c00570f0a48175d2
Tal  Leibman <tall@buzzilla.com>
2020-10-17 13:57:17 +0300

    mysql_client.py: create all tables at start up

--- db/__init__.py
+++ db/__init__.py
@@ -1,11 +1,13 @@
 import os
 
 from .mysql_client import MySqlClient, SqlTableDeclarativeBase
-from .models import Message, User,MessageResponse
+from .models import Message, User, MessageResponse, ParseWriteMessageRequest
 
 mysql_client_instance = MySqlClient.get_mysql_client(
     host=os.environ["MYSQL_HOST"],
     user=os.environ["MYSQL_USER"],
     password=os.environ["MYSQL_PASSWORD"],
-    scheme="messages_app",
+    scheme=os.environ["mysql_schema"],
 )
+
+mysql_client_instance.create_all_tables()
