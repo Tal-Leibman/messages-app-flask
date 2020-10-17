418ceaa9ccfc4163faeb097519c297129888dba5
Tal  Leibman <tall@buzzilla.com>
2020-10-17 15:33:25 +0300

    users.py: simple register route to add user no auth

--- controllers/users.py
+++ controllers/users.py
@@ -1,7 +1,21 @@
 import logging
 
 from flask import Blueprint, request, abort
-from db import mysql_client_instance
+from sqlalchemy.orm import Session
+
+from db import sql_client_instance, User
 
 log = logging.getLogger(__name__)
 users_bp = Blueprint("users", __name__)
+
+
+@users_bp.route("/register", methods=["POST"])
+def register():
+    request_body = request.json
+    with sql_client_instance.session_scope() as s:
+        s: Session
+        user = s.query(User).filter(User.email == request_body["email"]).first()
+        if user:
+            return abort(400)
+        s.add((User(email=request_body["email"])))
+    return {"status": "ok"}
