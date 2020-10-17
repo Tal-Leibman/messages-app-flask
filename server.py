3f38a2431dd00dbd494a28e102fd39dd8f436151
Tal  Leibman <tall@buzzilla.com>
2020-10-17 16:27:19 +0300

    remove host

--- server.py
+++ server.py
@@ -1,10 +1,8 @@
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     import os
     from main import app
     import waitress
+
     port = os.environ["PORT"]
-    host = os.environ["HOST"]
-    print(f"start server {host=} {port=}")
-    waitress.serve(app,host=host,port=port)
+    print(f"start server {port=}")
+    waitress.serve(app, port=port)
